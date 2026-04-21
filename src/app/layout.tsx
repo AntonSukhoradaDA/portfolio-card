08256884d364d172f680f0082e0d53282de9ade5
AntonSukhoradaDA <Anton.Sukhorada@dataart.com>
2026-04-21 16:14:52 +0300

    [PC-5]: Website improvements

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,26 +3,19 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({
-    weight: "400",
-    subsets: ["latin"],
+  weight: "400",
+  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-    title: "Anton Sukhorada",
-    description: "Anton Sukhorada Developer From Kyiv",
-    icons: {
-        icon: "/icon.png",
-    },
+  title: "Anton Sukhorada",
+  description: "Anton Sukhorada Developer From Kyiv",
 };
 
-export default function RootLayout({
-                                       children,
-                                   }: {
-    children: React.ReactNode;
-}) {
-    return (
-        <html lang="en">
-        <body className={inter.className}>{children}</body>
-        </html>
-    );
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,45 +1,49 @@
-"use client";
-
 export default function Home() {
-    return (
-        <div
-            className="bg-white overflow-hidden flex flex-col w-full h-full tracking-tighter text-black lg:text-2xl text-lg lg:pt-16 lg:pl-16 p-4 gap-16">
-            <div className="flex items-start justify-center w-full flex-col">
-                <h1>Anton Sukhorada</h1>
-                <h2>senior software engineer</h2>
-            </div>
-            <div className="flex items-start justify-center w-full flex-col">
-                <p>{`with 6 years of experience, I’ve built and shipped software `}</p>
-                <p> in corporations, startups, and as an entrepreneur.</p>
-            </div>
-            <div className="flex items-start justify-center w-full flex-col">
-                <p>{`currently i'm working as senior full-stack engineer`}</p>
-                <p>and living in Kyiv.</p>
-            </div>
-            <div className="flex items-start justify-center w-full flex-col">
-                <p>i share my life on <a className="text-blue-600" href="https://instagram.com/asukhorada" target="_blank">
-                    instagram
-                </a>.</p>
-            </div>
-            <div className="flex items-start justify-center w-full flex-col">
-                <p>business enquires on <a className="text-blue-600" href="https://linkedin.com/in/sukhorada-anton/" target="_blank">
-                    linkedin
-                </a>.</p>
-            </div>
-            <div className="flex items-start justify-center w-full flex-col">
-                <p>my work can be found on <a className="text-blue-600" href="https://github.com/AntonSukhoradaDA" target="_blank">
-                    github
-                </a>.</p>
-            </div>
-            <div className="flex items-start justify-center w-full flex-col">
-                <p>contact me at <a className="text-blue-600" href="mailto:antonsukhorada@gmail.com" target="_blank">
-                    antonsukhorada@gmail.com
-                </a></p>
-            </div>
-            <div className="flex items-start justify-center w-full flex-col">
-                <p>Cheers,</p>
-                <p>Anton</p>
-            </div>
-        </div>
-    );
+  return (
+    <div className="flex flex-col tracking-tighter text-black lg:text-2xl text-lg lg:pt-16 lg:pl-16 p-4 gap-16">
+      <div>
+        <h1>Anton Sukhorada</h1>
+        <p>senior software engineer</p>
+      </div>
+      <div>
+        <p>with 6 years of experience, I've built and shipped software</p>
+        <p>in corporations, startups, and as an entrepreneur.</p>
+      </div>
+      <div>
+        <p>currently i'm working as senior full-stack engineer</p>
+        <p>and living in Kyiv.</p>
+      </div>
+      <p>
+        i share my life on{" "}
+        <a className="text-blue-600" href="https://instagram.com/asukhorada" target="_blank">
+          instagram
+        </a>
+        .
+      </p>
+      <p>
+        business enquires on{" "}
+        <a className="text-blue-600" href="https://linkedin.com/in/sukhorada-anton/" target="_blank">
+          linkedin
+        </a>
+        .
+      </p>
+      <p>
+        my work can be found on{" "}
+        <a className="text-blue-600" href="https://github.com/AntonSukhoradaDA" target="_blank">
+          github
+        </a>
+        .
+      </p>
+      <p>
+        contact me at{" "}
+        <a className="text-blue-600" href="mailto:antonsukhorada@gmail.com" target="_blank">
+          antonsukhorada@gmail.com
+        </a>
+      </p>
+      <div>
+        <p>Cheers,</p>
+        <p>Anton</p>
+      </div>
+    </div>
+  );
 }
